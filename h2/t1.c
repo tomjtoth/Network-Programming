927e1d7d584398845b271d44f87f33cd0d817fd8
Tamás Tóth <tomjtoth@gmail.com>
2026-01-24 17:16:28 +0200

    added conditional usleep

--- h2/t1.c
+++ h2/t1.c
@@ -41,9 +41,12 @@ int main(int argc, char *argv[])
     for (i = 1; i <= 5; i++)
     {
         int len = sprintf(buf, "i = %d (PID %d)\n", i, getpid());
-
         write(STDOUT_FILENO, buf, len);
-        usleep(50000);
+
+#ifdef SLEEP
+        // sleep only when compiled with `-DSLEEP=500000` flag
+        usleep(SLEEP);
+#endif
     }
 
     exit(0);
