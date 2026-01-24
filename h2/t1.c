0789a4531616c02177dc3c3561d31bf138a10b66
Tamás Tóth <tomjtoth@gmail.com>
2026-01-24 16:50:23 +0200

    h2t1a

--- h2/t1.c
+++ h2/t1.c
@@ -3,11 +3,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUFSIZE 256
+
 int main()
 {
     int i;
+    char buf[BUFSIZE];
+
     fork();
     for (i = 1; i <= 5; i++)
-        printf(" i = %d \n", i);
+    {
+        int len = sprintf(buf, "i = %d (PID %d)\n", i, getpid());
+
+        write(STDOUT_FILENO, buf, len);
+        usleep(50000);
+    }
+
     exit(0);
 }
