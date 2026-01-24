648761b6eab181194816da42018c6db29a07aed5
Tamás Tóth <tomjtoth@gmail.com>
2026-01-24 17:13:35 +0200

    h2t1b,c

--- h2/t1.c
+++ h2/t1.c
@@ -2,15 +2,42 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define BUFSIZE 256
 
-int main()
+int main(int argc, char *argv[])
 {
-    int i;
+    int i, p, num_procs = 5;
+    pid_t pid;
     char buf[BUFSIZE];
 
-    fork();
+    if (argc > 1)
+    {
+        num_procs = atoi(argv[1]);
+        if (num_procs < 1 || num_procs > 50)
+        {
+            const char *msg = "1st arg must be 1-50 if passed\n";
+            write(STDERR_FILENO, msg, strlen(msg));
+            exit(1);
+        }
+    }
+
+    // create N child processes
+    for (p = 1; p < num_procs; p++)
+    {
+        pid = fork();
+        if (pid < 0)
+        {
+            perror("fork failed");
+            exit(1);
+        }
+
+        // child breaks the loop, only the parent forks
+        if (pid == 0)
+            break;
+    }
+
     for (i = 1; i <= 5; i++)
     {
         int len = sprintf(buf, "i = %d (PID %d)\n", i, getpid());
