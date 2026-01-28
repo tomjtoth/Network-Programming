bf8289c212ba1ba44ded65f7ad966991a40a1b33
Tamás Tóth <tomjtoth@gmail.com>
2026-01-28 15:42:16 +0200

    t4 fixes

--- h2/t4.c
+++ h2/t4.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -8,7 +9,14 @@ void sig_chld(int signo)
     pid_t pid;
     int stat;
 
+    int prevno = errno;
+
     while ((pid = waitpid(-1, &stat, WNOHANG)) > 0)
-        // https://man7.org/linux/man-pages/man7/signal-safety.7.html
-        write(STDOUT_FILENO, "child %d terminated\n", pid);
+    // https://man7.org/linux/man-pages/man7/signal-safety.7.html
+    {
+        const char *msg = "child %d terminated\n";
+        write(STDERR_FILENO, msg, strlen(msg));
+    }
+
+    errno = prevno;
 }
