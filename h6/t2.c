8d3f54a8d53fe365820bb814cb2b8c4c1ebb43f1
Tamás Tóth <tomjtoth@gmail.com>
2026-02-24 15:43:28 +0200

    simpler example syslog usage

--- h6/t2.c
+++ h6/t2.c
@@ -1,33 +1,16 @@
-#define _POSIX_C_SOURCE 200112L
-
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <syslog.h>
-#include <errno.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
+int main(void)
 {
-    if (argc != 2)
-        fprintf(stderr, "correct usage: %s <path-to-file>\n", argv[0]);
-
     openlog("h6t2", LOG_PID | LOG_CONS, LOG_USER);
 
-    syslog(LOG_INFO, "Program started");
+    syslog(LOG_INFO, "this is an info");
 
-    FILE *f = fopen(argv[1], "r");
-    if (!f)
-    {
-        syslog(LOG_ERR, "Failed to open file: %s", strerror(errno));
-    }
-    else
-    {
-        syslog(LOG_INFO, "File opened successfully");
-        fclose(f);
-    }
+    syslog(LOG_ERR, "this is a formatted error message: '%s'", "from parameter");
 
-    syslog(LOG_DEBUG, "Program finishing normally");
+    syslog(LOG_DEBUG, "this is debug info");
 
     closelog();
 
